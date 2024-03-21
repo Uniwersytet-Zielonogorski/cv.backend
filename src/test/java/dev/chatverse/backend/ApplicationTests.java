f7a8b4bafb0e24bd69ce6a302d8827a9306da11d
Grzegorz Dziedzic <grzegorzdziedzic198@gmail.com>
2024-03-21 16:05:57 +0100

    test: Services and Controllers
    MockMvc tests do not reflect reality idk why. Such testing is pointless. It can be caused of spring behavior idk.
    Another issues are beans annotation. @SpringBootTest need context that require repositories and others beans
    
    BREAKING CHANGE: Skill issue

--- src/test/java/dev/chatverse/backend/ApplicationTests.java
+++ src/test/java/dev/chatverse/backend/ApplicationTests.java
@@ -3,10 +3,17 @@ package dev.chatverse.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * TODO
+ *  Failed to load ApplicationContext
+ *  Error creating bean with name 'webSecurityConfig' when @SpringBootTest is used
+ *
+ */
 @SpringBootTest
 class ApplicationTests {
 
     @Test
     void contextLoads() {
     }
+
 }
